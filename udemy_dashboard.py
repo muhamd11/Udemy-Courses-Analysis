506139441049d4531707fe0724a7dac44c9c0533
Muhamd Medhat <114123913+muhamd11@users.noreply.github.com>
2023-03-16 17:50:39 +0200

    Update udemy_dashboard.py

--- udemy_dashboard.py
+++ udemy_dashboard.py
@@ -1,10 +1,10 @@
 # importing libraries
- import streamlit as st
- import numpy as np 
- import pandas as pd
- import matplotlib.pyplot as plt 
- import seaborn as sns 
- import plotly.express as px
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import plotly.express as px
 
                           
 st.set_page_config(layout='centered')
